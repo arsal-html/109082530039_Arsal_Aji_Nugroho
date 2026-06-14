32cb360237373ea7177514a1e52c5559d41be170
arsal-html <arsalaji.134@gmail.com>
2026-06-14 17:47:22 +0700

    done

--- modul 14 selection/median.go
+++ modul 14 selection/median.go
@@ -4,17 +4,15 @@ import "fmt"
 
 type arr [1000000]int
 
-func insertionSort(T *arr, n int) {
-	i := 1
-	for i <= n-1 {
-		j := i
-		temp := T[j]
-		for j > 0 && temp < T[j-1] {
-			T[j] = T[j-1]
-			j = j - 1
+func selectionSort(T *arr, n int) {
+	for i := 0; i < n-1; i++ {
+		minIdx := i
+		for j := i + 1; j < n; j++ {
+			if T[j] < T[minIdx] {
+				minIdx = j
+			}
 		}
-		T[j] = temp
-		i = i + 1
+		T[i], T[minIdx] = T[minIdx], T[i]
 	}
 }
 
@@ -29,23 +27,17 @@ func hitungMedian(T *arr, n int) int {
 }
 
 func main() {
-	var data arr 
+	var data arr
 	var angka, n int
-
 	fmt.Scan(&angka)
-
 	for angka != -5313 {
 		if angka == 0 {
-	
-			insertionSort(&data, n)
-			
+			selectionSort(&data, n)
 			fmt.Println(hitungMedian(&data, n))
 		} else {
-			
 			data[n] = angka
 			n = n + 1
 		}
-		
 		fmt.Scan(&angka)
 	}
 }
